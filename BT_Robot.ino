9a43cfe3178b7eb69ffa11ac549dce1ee7bfe552
aluelis <svetlintanyi@gmail.com>
2016-05-03 15:46:50 +0200

    New control string
    Adjusted code to support it.

--- BT_Robot.ino
+++ BT_Robot.ino
@@ -3,14 +3,10 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
-
-char commandDirection[1];
-int commandSpeed;
-boolean commandBool;
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1); //left back
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2); //right back
+Adafruit_DCMotor *motor3 = AFMS.getMotor(3); //right front
+Adafruit_DCMotor *motor4 = AFMS.getMotor(4); //left front
 
 void setup() {
   Serial.begin(9600);
@@ -21,100 +17,56 @@ void setup() {
 void loop() {
 
   if (Serial.available() > 0) {
-    String command = Serial.readString();
-    Serial.println("CommandString " + command);
-
-    command.substring(0,1).toCharArray(commandDirection,1);
-    Serial.println("CommandDirection " + commandDirection[0]);
-    commandSpeed = command.substring(1,3).toInt();
-    Serial.println("CommandSpeed " + commandSpeed);
-    
-    
-      if (commandDirection[0] == 'F') {
-      moveForward(commandSpeed, true);
-      } else if (commandDirection[0] == 'R') {
-      turn(commandSpeed, true);
-      } else if (commandDirection[0] == 'L') {
-      turn(commandSpeed, false);
-      } else if (commandDirection[0] == 'B') {
-      moveForward(commandSpeed, false);
-      }else if (commandDirection[0] == 'S') {
-      moveForward(0, true);
-      }/*
-    while (command == 'F') {
-      moveForward(255, true);
-    }
-    while (command == 'S') {
-      moveForward(0, true);
-    }*/
+    String command = Serial.readStringUntil('\n');
+    String leftSpeedString = command.substring(0, 4);
+    String rightSpeedString = command.substring(4, 8);
+    int left = leftSpeedString.toInt();
+    int right = rightSpeedString.toInt();
+    move(left, right);
   }
-
-  /*
-      switch (command) {
-        case 'F':
-          moveForward(255, true);
-          break;
-        case 'R':
-          turn(255, true);
-          break;
-        case 'L':
-          turn(255, false);
-          break;
-        case 'B':
-          moveForward(255, false);
-          break;
-        case 'S':
-          moveForward(0, true);
-          break;
-        default:
-          // if nothing matches, do nothing
-          break;
-      }
-    }
-  */
-
 }
 
-void moveForward(int speedBot, boolean forward) {
+void move(int leftSpeed, int rightSpeed) {
 
-  
+  //MOTOR 1,4 LEFT
+  //MOTOR 2,3 RIGHT
+  boolean left, right;
 
-  motor1->setSpeed (speedBot);
-  motor2->setSpeed (speedBot);
-  motor3->setSpeed (speedBot);
-  motor4->setSpeed (speedBot);
-
-  if (forward) {
-    Serial.println("Moved forward");
-    motor1->run(FORWARD);
-    motor2->run(FORWARD);
-    motor3->run(FORWARD);
-    motor4->run(FORWARD);
+  if (leftSpeed <= 0) {
+    left = true;
+    //   Serial.println("left = true");
   } else {
-    motor1->run(BACKWARD);
-    motor2->run(BACKWARD);
-    motor3->run(BACKWARD);
-    motor4->run(BACKWARD);
+    left = false;
   }
-}
+  if (rightSpeed <= 0) {
+    right = true;
+    //  Serial.println("right = true");
+  } else {
+    right = false;
+  }
 
-void turn(int speedBot, boolean right) {
+  leftSpeed = abs(leftSpeed);
+  rightSpeed = abs(rightSpeed);
+  
+  motor1->setSpeed (leftSpeed);
+  motor2->setSpeed (rightSpeed);
+  motor3->setSpeed (rightSpeed);
+  motor4->setSpeed (leftSpeed);
 
-  motor1->setSpeed (speedBot);
-  motor2->setSpeed (speedBot);
-  motor3->setSpeed (speedBot);
-  motor4->setSpeed (speedBot);
+  if (left) {
+    motor1->run(BACKWARD);
+    motor4->run(BACKWARD);
+  } else {
+    motor1->run(FORWARD);
+    motor4->run(FORWARD);
+  }
 
   if (right) {
-    motor1->run(FORWARD);
-    motor4->run(FORWARD);
-    motor2->run (BACKWARD);
-    motor3->run (BACKWARD);
+    motor2->run(BACKWARD);
+    motor3->run(BACKWARD);
   } else {
-    motor1->run(BACKWARD);
-    motor4->run(BACKWARD);
-    motor2->run (FORWARD);
-    motor3->run (FORWARD);
+    motor2->run(FORWARD);
+    motor3->run(FORWARD);
   }
 }
 
